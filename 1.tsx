759bbbd577818470b64f122272b8fdc7b3e02da3
Akshat Chaube <92796050+Akshatchaube01@users.noreply.github.com>
2025-04-28 14:35:09 +0530

    Update 1.tsx

--- 1.tsx
+++ 1.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Cell } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Cell, PieChart, Pie } from 'recharts';
 
 interface DataItem {
   name: string;
   value: number;
 }
 
-const DynamicBarChart: FC = () => {
+const DynamicCharts: FC = () => {
   const data: DataItem[] = [
     { name: 'A', value: 400 },
     { name: 'B', value: 300 },
@@ -14,29 +14,53 @@ const DynamicBarChart: FC = () => {
     { name: 'D', value: 100 }
   ];
 
-  const colors = ['#8884d8', '#82ca9d', '#ffc658', '#ff7f50'];
+  const colors: string[] = ['#8884d8', '#82ca9d', '#ffc658', '#ff7f50'];
 
   return (
-    <div className="w-full h-96 p-4">
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          data={data}
-          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="value" barSize={30}>
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-            ))}
-          </Bar>
-        </BarChart>
-      </ResponsiveContainer>
+    <div className="w-full flex flex-col md:flex-row gap-8 p-4">
+      <div className="w-full md:w-1/2 h-96">
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            data={data}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="value" barSize={30}>
+              {data.map((_, index) => (
+                <Cell key={`cell-bar-${index}`} fill={colors[index % colors.length]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+
+      <div className="w-full md:w-1/2 h-96">
+        <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              label
+            >
+              {data.map((_, index) => (
+                <Cell key={`cell-pie-${index}`} fill={colors[index % colors.length]} />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
 
-export default DynamicBarChart;
+export default DynamicCharts;
