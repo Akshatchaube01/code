aebeb53281dd638a2d497a18455bba24bad04fe7
Akshat Chaube <92796050+Akshatchaube01@users.noreply.github.com>
2025-04-30 00:11:32 +0530

    Update 1.tsx

--- 1.tsx
+++ 1.tsx
@@ -1,51 +1,78 @@
-<GCBTable
-  data={[
-    {
-      country: 'India',
-      gcb3: 1,
-      gcb4: 11,
-      gcb5: 27,
-      gcb6: 41,
-      gcbNA: 0,
-      total: 80,
-      children: [
-        {
-          country: 'Chennai',
-          gcb3: 0,
-          gcb4: 5,
-          gcb5: 15,
-          gcb6: 20,
-          gcbNA: 0,
-          total: 40,
-        },
-        {
-          country: 'Mumbai',
-          gcb3: 1,
-          gcb4: 6,
-          gcb5: 12,
-          gcb6: 21,
-          gcbNA: 0,
-          total: 40,
-        },
-      ],
-    },
-    {
-      country: 'United Kingdom',
-      gcb3: 2,
-      gcb4: 5,
-      gcb5: 9,
-      gcb6: 0,
-      gcbNA: 2,
-      total: 18,
-    },
-    {
-      country: 'Total',
-      gcb3: 3,
-      gcb4: 16,
-      gcb5: 36,
-      gcb6: 41,
-      gcbNA: 2,
-      total: 98,
-    },
-  ]}
-/>
+"use client";
+
+import { FC } from "react";
+import { TrendingUp } from "lucide-react";
+import { Bar, BarChart, XAxis, YAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/frame/ui/card";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/frame/ui/chart";
+
+type DataType = {
+  browser: string;
+  visitors: number;
+  fill: string;
+};
+
+type DynamicBarChartProps = {
+  data: DataType[];
+  config: ChartConfig;
+};
+
+const DynamicBarChart: FC<DynamicBarChartProps> = ({ data, config }) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Bar Chart Mixed</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={config}>
+          <BarChart
+            data={data}
+            layout="vertical"
+            margin={{ left: 0 }}
+            width={500}
+            height={300}
+          >
+            <YAxis
+              dataKey="browser"
+              type="category"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) =>
+                config[value as keyof typeof config]?.label || value
+              }
+            />
+            <XAxis dataKey="visitors" type="number" hide />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+            <Bar dataKey="visitors" radius={5} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default DynamicBarChart;
