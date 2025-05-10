5e209968fc81f69609cd90f296d9b29ac01331ae
Akshat Chaube <92796050+Akshatchaube01@users.noreply.github.com>
2025-05-10 21:39:06 +0530

    Update 1.tsx

--- 1.tsx
+++ 1.tsx
@@ -1,21 +1,38 @@
-<Autocomplete
-  multiple
-  options={projectLeads}
-  getOptionLabel={(option) => option.name}
-  value={project_lead2}
-  onChange={(event, newValue) => setProjectLead2(newValue)}
-  renderInput={(params) => (
-    <TextField
-      {...params}
-      placeholder="Select Project Lead 2"
-      variant="outlined"
-      required
-      error={!project_lead2 || project_lead2.length === 0}
-      helperText={
-        !project_lead2 || project_lead2.length === 0
-          ? "Project Lead 2 is required"
-          : ""
-      }
-    />
-  )}
-/>
+const columns = [
+  {
+    title: 'Assignment ID',
+    field: 'assignment_id',
+    sorter: 'number',
+    headerFilter: 'input',
+  },
+  {
+    title: 'PSID',
+    field: 'psid',
+    sorter: 'string',
+    headerFilter: 'input',
+  },
+  {
+    title: 'Assigned FTE',
+    field: 'assigned_fte',
+    sorter: 'number',
+    headerFilter: 'input',
+    editable: (cell) => !cell.getRow().getData().disable,
+  },
+  {
+    title: 'Start Date',
+    field: 'start_date',
+    sorter: 'string',
+    headerFilter: 'input',
+    editable: (cell) => !cell.getRow().getData().disable,
+  },
+  {
+    title: 'End Date',
+    field: 'end_date',
+    sorter: 'string',
+    headerFilter: 'input',
+    cellStyled: (cell) => {
+      const data = cell.getRow().getData();
+      return data.is_past ? { backgroundColor: 'red', color: 'white' } : {};
+    },
+  },
+];
