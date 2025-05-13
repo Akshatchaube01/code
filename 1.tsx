00d12816013e767ba064d4652e8c4e890c9dbf9b
Akshat Chaube <92796050+Akshatchaube01@users.noreply.github.com>
2025-05-13 22:04:33 +0530

    Update 1.tsx

--- 1.tsx
+++ 1.tsx
@@ -1,57 +1,19 @@
-const RenderAutocomplete = (
-  label: string,
-  options: Option[],
-  selected: Option[],
-  key: keyof typeof filters
-) => {
-  const isAllSelected = selected.length === options.length;
+function convertProjectStatusToMetrics(data: Record<string, number>) {
+  const colorVars = [
+    "var(--color-chrome)",
+    "var(--color-safari)",
+    "var(--color-firefox)",
+    "var(--color-edge)",
+    "var(--color-opera)",
+    "var(--color-brave)",
+    "var(--color-vivaldi)",
+  ];
 
-  const handleChange = (_: any, newValue: Option[]) => {
-    const isSelectAll = newValue.some((opt) => opt.id === -1);
-    const newSelection =
-      isSelectAll && isAllSelected
-        ? []
-        : isSelectAll
-        ? options
-        : newValue.filter((o) => o.id !== -1);
-
-    setFilters((prev) => ({
-      ...prev,
-      [key]: newSelection,
-    }));
-  };
-
-  return (
-    <Autocomplete
-      multiple
-      disableCloseOnSelect
-      options={[{ id: -1, name: "Select All" }, ...options]}
-      value={isAllSelected ? [...options] : selected}
-      onChange={handleChange}
-      getOptionLabel={(option) => option.name}
-      renderOption={(props, option) => {
-        const isSelectAll = option.id === -1;
-        const isChecked = isSelectAll
-          ? selected.length > 0 && selected.length < options.length
-            ? 'indeterminate'
-            : selected.length === options.length
-          : selected.some((s) => s.id === option.id);
-
-        return (
-          <li key={option.id} {...props}>
-            <input
-              type="checkbox"
-              checked={isChecked}
-              readOnly
-              style={{ marginRight: 8 }}
-            />
-            {option.name}
-          </li>
-        );
-      }}
-      renderInput={(params) => <TextField {...params} label={label} />}
-      isOptionEqualToValue={(opt, val) => opt.id === val.id}
-      className='bg-white rounded'
-    />
-  );
-};
+  const entries = Object.entries(data);
+  
+  return entries.map(([status, value], index) => ({
+    metric: status,
+    value,
+    fill: colorVars[index % colorVars.length],
+  }));
+}
